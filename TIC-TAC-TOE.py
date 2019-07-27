80a227dfa6f7b3c58ba7273fe218c7c514476ed9
Lohitya <44522008+ambashtalk@users.noreply.github.com>
2019-07-27 16:48:40 +0530

    Python 3 code
    Copy and paste code. Then run in console.

--- TIC-TAC-TOE.py
+++ TIC-TAC-TOE.py
@@ -1,34 +1,11 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# # Milestone Project 1
-# ## Congratulations on making it to your first milestone!
-# You've already learned a ton and are ready to work on a real project.
-# 
-# Your assignment: Create a Tic Tac Toe game. You are free to use any IDE you like.
-# 
 # Here are the requirements:
 # 
 # * 2 players should be able to play the game (both sitting at the same computer)
 # * The board should be printed out every time a player makes a move
 # * You should be able to accept input of the player position and then place a symbol on the board
 # 
-# Feel free to use Google to help you figure anything out (but don't just Google "Tic Tac Toe in Python" otherwise you won't learn anything!) Keep in mind that this project can take anywhere between several hours to several days.
-# 
-# There are 4 Jupyter Notebooks related to this assignment:
-# 
-# * This Assignment Notebook
-# * A "Walkthrough Steps Workbook" Notebook
-# * A "Complete Walkthrough Solution" Notebook
-# * An "Advanced Solution" Notebook
-# 
-# I encourage you to just try to start the project on your own without referencing any of the notebooks. If you get stuck, check out the next lecture which is a text lecture with helpful hints and steps. If you're still stuck after that, then check out the Walkthrough Steps Workbook, which breaks up the project in steps for you to solve. Still stuck? Then check out the Complete Walkthrough Solution video for more help on approaching the project!
 
-# There are parts of this that will be a struggle...and that is good! I have complete faith that if you have made it this far through the course you have all the tools and knowledge to tackle this project. Remember, it's totally open book, so take your time, do a little research, and remember:
-# 
-# ## HAVE FUN!
-
-# In[ ]:
+# replace "os.system('cls')" wiht "clear_output()" if you need to run in Jupyter Notebook
 
 
 from IPython.display import clear_output
